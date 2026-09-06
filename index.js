98761485799e40fea00c30d62190f87d427fea60
Andriy <andrijmilevskij@gmail.com>
2026-09-06 12:27:00 +0300

    add index

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+console.log(import.meta.dirname);
+
+const carsPath = path.join(import.meta.dirname, "assets", "cars.json");
+const carsData = await fs.readFile(carsPath, "utf8");
+
+const cars = JSON.parse(carsData);
+
+console.log(cars);
 
 
-const message = 'Hello world very good gfhjjn';
+// const carsData =await fs.readFile("./assets/cars.json", "utf8");
 
-console.log(message);
+// const cars = JSON.parse(carsData);
+// console.log(typeof cars);
