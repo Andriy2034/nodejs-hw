d579d0efe8eb173d6c438f86c004c25614f38f5d
Andriy <andrijmilevskij@gmail.com>
2026-09-06 12:42:35 +0300

    expres

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const cars = JSON.parse(carsData);
 
 console.log(cars);
 
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
 
 // const carsData =await fs.readFile("./assets/cars.json", "utf8");
 
