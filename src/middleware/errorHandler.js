03851fae07725d866b4077f29974c5f14a2a6146
Andriy <andrijmilevskij@gmail.com>
2026-09-13 20:24:20 +0300

    fix status code

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -4,13 +4,14 @@ import { isHttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
   if (isHttpError(err)) {
-    return res.status(err.statusCode).json({
+    return res.status(err.status).json({
       message: err.message,
     });
   }
 
+  const isPod = process.env.NODE_ENV === 'production';
 
   res.status(500).json({
-    message:  err.message
+    message: isPod ? err.message : err.stack,
   });
 };
